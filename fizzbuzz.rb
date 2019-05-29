b1dbd3654fb17b5d292a539012cad3f813522da7
Amritpal Singh <singhamritpal49@gmail.com>
2019-05-29 02:22:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,6 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+    "FizzBuzz" # Go fizz
   end
 end
